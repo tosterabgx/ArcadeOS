40971c7009c038fb93b5c433a5411ea8b0473718
tosterabgx <misiurenko.arsenii@gmail.com>
2026-06-14 02:35:32 +0300

    Refactor window component structure and add drag functionality

--- script.js
+++ script.js
@@ -5,3 +5,70 @@ function updateTime() {
 }
 
 setInterval(updateTime, 1000);
+
+
+dragElement(document.getElementById("welcome"));
+
+let welcomeScreen = document.querySelector("#welcome")
+
+let welcomeScreenClose = document.querySelector("#welcome-close");
+let welcomeScreenOpen = document.querySelector("#welcome-open");
+
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
+
+
+function dragElement(element) {
+  let initialX = 0;
+  let initialY = 0;
+  let currentX = 0;
+  let currentY = 0;
+
+  if (document.getElementById(element.id + "-header")) {
+    document.getElementById(element.id + "-header").onmousedown = startDragging;
+  } else {
+    element.onmousedown = startDragging;
+  }
+
+  function startDragging(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    document.onmouseup = stopDragging;
+    document.onmousemove = dragElement;
+  }
+
+  function dragElement(e) {
+    e = e || window.event;
+    e.preventDefault();
+
+    currentX = initialX - e.clientX;
+    currentY = initialY - e.clientY;
+    initialX = e.clientX;
+    initialY = e.clientY;
+
+    element.style.top = (element.offsetTop - currentY) + "px";
+    element.style.left = (element.offsetLeft - currentX) + "px";
+  }
+
+  function stopDragging() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+function closeWindow(element) {
+  element.style.display = "none"
+}
+
+function openWindow(element) {
+  element.style.display = "block"
+}
