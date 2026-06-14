04650ebad1248996eb80e65bab0e4d2cd18267df
tosterabgx <misiurenko.arsenii@gmail.com>
2026-06-14 13:53:56 +0300

    Add desktop icons + sample game window (without content)

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-function updateTime() {
-    let currentTime = new Date().toLocaleTimeString();
-    let timeText = document.querySelector("#time");
-    timeText.innerHTML = currentTime;
+let startTime = new Date();
+
+function updateClock() {
+  let currentTime = new Date().toLocaleTimeString();
+  document.querySelector("#clock").innerHTML = currentTime;
 }
 
-setInterval(updateTime, 1000);
+function updatePlayedTime() {
+  let currentTime = new Date();
+  let elapsedTime = Math.floor((currentTime - startTime) / 60000);
+  elapsedTime += " minute" + (elapsedTime == 1 ? "" : "s");
+  document.querySelector("#time-played").innerHTML = elapsedTime;
+}
+
+setInterval(updateClock, 1000);
+setInterval(updatePlayedTime, 60000);
 
 
-dragElement(document.getElementById("welcome"));
+let biggestIndex = 1;
+let topBar = document.querySelector("#topbar");
 
-let welcomeScreen = document.querySelector("#welcome")
+for (const element of document.querySelectorAll(".window")) {
+  dragElement(element);
 
-let welcomeScreenClose = document.querySelector("#welcome-close");
-let welcomeScreenOpen = document.querySelector("#welcome-open");
+  element.addEventListener("mousedown", () => handleWindowTap(element));
+  document.getElementById(element.id + "-close").addEventListener("click", () => closeWindow(element));
+}
 
-welcomeScreenClose.addEventListener("click", function() {
-  closeWindow(welcomeScreen);
-});
-
-welcomeScreenOpen.addEventListener("click", function() {
-  openWindow(welcomeScreen);
-});
-
+for (const icon of document.querySelectorAll(".icon")) {
+  let element = document.getElementById(icon.id.replace("-icon", ""));
+  icon.addEventListener("click", () => openWindow(element));
+}
 
 function dragElement(element) {
   let initialX = 0;
@@ -66,9 +74,16 @@ function dragElement(element) {
 }
 
 function closeWindow(element) {
-  element.style.display = "none"
+  element.style.display = "none";
 }
 
 function openWindow(element) {
-  element.style.display = "block"
+  element.style.display = "block";
+  handleWindowTap(element)
+}
+
+function handleWindowTap(element) {
+  biggestIndex++;
+  element.style.zIndex = biggestIndex;
+  topBar.style.zIndex = biggestIndex + 1;
 }
